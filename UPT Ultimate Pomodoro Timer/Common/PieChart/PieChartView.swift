c58d68c158c40b20f26182368c2f884a5bc9ada1
Stepan Haritonov <stepan.haritonov@simbirsoft.com>
2023-11-24 15:23:27 +0400

    Тестовая реализация статичного блока визуального отображения периода времени Разработка адаптивного переиспользуемого динамичного способа создания блока отображения времени

--- UPT Ultimate Pomodoro Timer/Common/PieChart/PieChartView.swift
+++ UPT Ultimate Pomodoro Timer/Common/PieChart/PieChartView.swift
@@ -19,40 +19,79 @@ public final class PieChartView: UIView {
     
     public override func draw(_ rect: CGRect) {
         super.draw(rect)
+        let fillColor = UIColor(red: 0.965, green: 0.976, blue: 0.894, alpha: 1.000)
         
-        let center = CGPoint(
-            x: rect.midX,
-            y: rect.midY
+        let shapeLayer = CAShapeLayer()
+        
+        let radian: CGFloat = 30.0 * (.pi/180.0)
+        let x = bounds.height/2 * cos(radian)
+        let y = bounds.height/2 * sin(radian)
+
+        //// Bezier Drawing
+        let bezierPath = UIBezierPath()
+        /// Обход контура по часовой стрелке
+        bezierPath.move(to: CGPoint(x: 150, y: 7))
+        /// Скругление угла
+        bezierPath.addCurve(
+            to: CGPoint(x: 157, y: 0.16),
+            controlPoint1: CGPoint(x: 150, y: 3.13),
+            controlPoint2: CGPoint(x: 153.14, y: -0.02)
         )
-        let radius = min(
-            rect.width,
-            rect.height
-        ) / 2
-        
-        var startAgle: CGFloat = -.pi / 2
-        
-        for segment in segments {
-            let endAngle = startAgle + (2 * .pi * (segment.value / 100))
-            
-            let path = UIBezierPath()
-            path.move(to: center)
-            path.addArc(
-                withCenter: center,
-                radius: radius,
-                startAngle: startAgle,
-                endAngle: endAngle,
-                clockwise: true
-            )
-            path.close()
-            
-            segment.color.setFill()
-            path.fill()
-            
-            startAgle = endAngle
-        }
+        /// Половина внешней дуги
+        bezierPath.addCurve(
+            to: CGPoint(x: 224.32, y: 19.71),
+            controlPoint1: CGPoint(x: 180.63, y: 1.27),
+            controlPoint2: CGPoint(x: 203.71, y: 7.95)
+        )
+        /// Вторая половина внешней дуги
+        bezierPath.addCurve(
+            to: CGPoint(x: 275.41, y: 67.7),
+            controlPoint1: CGPoint(x: 244.93, y: 31.46),
+            controlPoint2: CGPoint(x: 262.43, y: 47.92)
+        )
+        /// Скругление
+        bezierPath.addCurve(
+            to: CGPoint(x: 273.09, y: 77.21),
+            controlPoint1: CGPoint(x: 277.53, y: 70.94),
+            controlPoint2: CGPoint(x: 276.41, y: 75.24)
+        )
+        /// Радиальная линия
+        bezierPath.addLine(to: CGPoint(x: 252.86, y: 89.17))
+        /// Скругление
+        bezierPath.addCurve(
+            to: CGPoint(x: 243.08, y: 86.82),
+            controlPoint1: CGPoint(x: 249.53, y: 91.14),
+            controlPoint2: CGPoint(x: 245.26, y: 90.02)
+        )
+        /// Половина внутренней дуги
+        bezierPath.addCurve(
+            to: CGPoint(x: 205.74, y: 52.28),
+            controlPoint1: CGPoint(x: 233.45, y: 72.63),
+            controlPoint2: CGPoint(x: 220.69, y: 60.81)
+        )
+        /// Вторая половина внутренней дуги
+        bezierPath.addCurve(
+            to: CGPoint(x: 157, y: 37.72),
+            controlPoint1: CGPoint(x: 190.79, y: 43.75),
+            controlPoint2: CGPoint(x: 174.11, y: 38.78)
+        )
+        /// Скругление
+        bezierPath.addCurve(
+            to: CGPoint(x: 150, y: 30.5),
+            controlPoint1: CGPoint(x: 153.14, y: 37.48),
+            controlPoint2: CGPoint(x: 150, y: 34.37)
+        )
+        /// Радиальная линия
+        bezierPath.addLine(to: CGPoint(x: 150, y: 7))
+        bezierPath.close()
+
+        shapeLayer.path = bezierPath.cgPath
+        shapeLayer.fillColor = fillColor.cgColor
+        layer.addSublayer(shapeLayer)
     }
     
     private func configureUI() {
         backgroundColor = .clear
+        layer.masksToBounds = false
     }
 }
